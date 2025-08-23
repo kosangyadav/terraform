bcf28edfe5572298953862edbf8a28c1844928a2
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-08-23 15:11:15 +0530

    scripts for installations

---
+++ instructions/installation/fedoraInstall.sh
@@ -0,0 +1,3 @@
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+sudo dnf -y install terraform
---
+++ instructions/installation/macOsInstall.sh
@@ -0,0 +1,8 @@
+
+# for MacOS...
+
+# First, install the HashiCorp tap, which is Hashicorp's official repository of all our Homebrew packages.
+brew tap hashicorp/tap
+
+# Now, install Terraform from hashicorp/tap/terraform.
+brew install hashicorp/tap/terraform
